1863f06e16e162082a23ace541765cf85eb3f5ab
Diego <di3g0d0ming05@gmail.com>
2018-03-19 08:11:10 -0300

    ajuste no calculo de horario

--- utility/utility.py
+++ utility/utility.py
@@ -2,8 +2,8 @@ from datetime import datetime, timedelta
 
 
 def difference_hours(time_start, time_end):
-    difference = datetime.strptime(time_end.strftime("%H%M%S"), "%H%M%S") - datetime.strptime(time_start.strftime("%H%M%S"), "%H%M%S")
+    difference = datetime.strptime(time_end, "%H%M%S") - datetime.strptime(time_start, "%H%M%S")
     return timedelta(minutes=difference.total_seconds() / 60)
 
-def bank_of_hours(time_start, hour_entry, time_end, hour_exit):
-   return str(difference_hours(time_start, hour_entry) + difference_hours(time_end, hour_exit))
+def bank_of_hours(time_start, time_end):   
+   return str(60 / timedelta(datetime.strptime('090000', "%H%M%S")) - difference_hours(time_start, time_end) )
